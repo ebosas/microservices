1cbd5cf67215dda7d6079125b9439bdbae3cf1d5
ebosas <ebosas@gmail.com>
2021-12-02 14:33:29 +0200

    Test pipeline [BuildAll]

--- web/react/esbuild.js
+++
@@ -1,8 +0,0 @@
-require('esbuild').buildSync({
-  entryPoints: ['src/index.tsx'],
-  bundle: true,
-  minify: true,
-  sourcemap: false,
-  // target: ['chrome58', 'firefox57', 'safari11', 'edge16'],
-  outdir: 'build',
-})
